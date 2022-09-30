cf4be01fc996ff264b9f68188b00f9dcccf4f98f
David <davidongaroh@gmail.com>
2022-09-30 22:46:08 +0300

    solved

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length:{minimum:250}
+    validates :summary, length:{maximum:250}
+    validates :category, inclusion: { in:['Fiction', 'Non-Fiction'] }    
+
+    validate :title_validator
+
+    def title_validator
+        if title
+            valid_title = title.downcase
+        keywords = ["won't believe", "secret", "top[0..9]", "guess"]
+        if keywords.select { |word| valid_title.scan(word).first }.all?{|w|w.nil? }
+        # keywords.map{|word| valid_title.include?(/#{word}/)}
+            errors.add(:title, "Title must be clickbait-y")
+        end
+        
+    end
+    end
 end
